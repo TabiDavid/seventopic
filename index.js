6d6c3fbf1a5fcd7133e523adad5709331c6b047b
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-11-04 13:29:19 -0500

    commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function change(){
     var colorOne = document.getElementById("change1");
-    if(colorOne.style.backgroundColor == "red"){
-    colorOne.style.backgroundColor = "yellow";
-    colorOne.style.backgroundColor = "black";
+    if(colorOne.style.backgroundColor == "black"){
+    colorOne.style.backgroundColor = "white";
+    colorOne.style.color = "#009BC4";
+}
+
+else if (colorOne.style.backgroundColor == "white"){
+    colorOne.style.backgroundColor = "#009BC4";
+    colorOne.style.color = "black";
 }
 else {
-    colorOne.style.backgroundColor = "red";
-colorOne.style.backgroundColor = "white";
+    colorOne.style.backgroundColor = "black";
+colorOne.style.color = "yellow";
 }
 }
 window.onload = function(){
